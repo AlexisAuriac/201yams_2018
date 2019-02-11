c369e852d4474a845d57a128ccbb1fe1b8d02f51
alexis <alexis.auriac@epitech.eu>
2019-02-11 22:03:02 +0100

    Redirection to correct combination function done

--- combinaisons.rb
+++ combinaisons.rb
@@ -9,4 +9,28 @@ module Combinaisons
             "yams"
         ]
     end
+
+    def Combinaisons.pair(dice, options)
+        puts("TODO")
+    end
+
+    def Combinaisons.three(dice, options)
+        puts("TODO")
+    end
+
+    def Combinaisons.four(dice, options)
+        puts("TODO")
+    end
+
+    def Combinaisons.full(dice, options)
+        puts("TODO")
+    end
+
+    def Combinaisons.straight(dice, options)
+        puts("TODO")
+    end
+
+    def Combinaisons.yams(dice, options)
+        puts("TODO")
+    end
 end
